687ce131a54f73bad6b5c16e0062dcb133fb6f73
Nazarena Aranda <nazarenaaranda06@gmail.com>
2024-12-13 15:37:07 +0000

    incorporation of the quick sort

--- sort.h
+++ sort.h
@@ -1,6 +1,6 @@
 #ifndef SORT_H
 #define SORT_H
-
+#include <stdio.h>
 #include <stddef.h>
 
 /**
@@ -18,10 +18,10 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-void print_array(const int *, size_t);
 void bubble_sort(int *, size_t);
 void insertion_sort_list(listint_t **list);
-void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void selection_sort(int *, size_t);
+void quick_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
 #endif
